2aee5d3a1305e0332d2a34149b6948e626ef9fa3
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-06-15 13:08:17 +0530

    started basics

---
+++ components/Hero.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Hero(){
+    return(
+        <div className="hero">
+            <div className="flex-1 pt-36 padding-x">
+                <h1 className="hero__title">
+                    Find, book or rent a car
+
+                </h1>
+            </div>
+
+        </div>
+    )
+}

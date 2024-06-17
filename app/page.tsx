a1296fcf7c1476af4d51653720a58e8b67f6d6df
alpranjal28 <144878447+alpranjal28@users.noreply.github.com>
2024-06-17 14:51:31 +0530

    starting catalogue

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <main className=" overflow-hidden">
       <Hero/>
+      <div className="mt-12 padding-x padding-y max-width">
+
+      </div>
     </main>
   );
 }
